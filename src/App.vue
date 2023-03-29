435bd3e86d7b26817ed44879ca5992ff32b7c20a
Wiktor Morawski <wiktor.morawski@recruitee.com>
2023-03-29 19:51:45 +0200

    feature: wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
+  <v-app id="inspire">
+    <v-app-bar app color="white" flat>
+      <v-container class="py-0 fill-height">
+        <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
 
-    <v-app-bar app>
-      <!-- -->
+        <v-btn v-for="link in links" :key="link" text>
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
     </v-app-bar>
 
-    <!-- Sizes your content based upon application components -->
-    <v-main>
-      <!-- Provides the application the proper gutter -->
-      <v-container fluid>
-        <!-- If using vue-router -->
-        <router-view></router-view>
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list color="transparent">
+                <v-list-item v-for="n in 5" :key="n" link>
+                  <v-list-item-content>
+                    <v-list-item-title> List Item {{ n }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item link color="grey lighten-4">
+                  <v-list-item-content>
+                    <v-list-item-title> Refresh</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
       </v-container>
     </v-main>
-
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
@@ -28,7 +63,7 @@ export default Vue.extend({
   name: "App",
 
   data: () => ({
-    //
+    links: ["Home", "Collection", "Decks", "Scanner"],
   }),
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
-<template></template>
+<template>
+  <hello-world />
+</template>
 
 <script lang="ts">
 import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "HomeView",
 
-  components: {},
+  components: {
+    HelloWorld,
+  },
 });
 </script>
