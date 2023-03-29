afe60515fab229956d5537e2d45e19f4aacfb69b
Wiktor Morawski <wiktor.morawski@recruitee.com>
2023-03-29 19:01:34 +0200

    chore: remove default styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer app>
-      <!-- -->
-    </v-navigation-drawer>
+    <v-navigation-drawer></v-navigation-drawer>
 
     <v-app-bar app>
       <!-- -->
